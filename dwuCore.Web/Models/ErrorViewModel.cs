6a343e3fc96aeb047a39722f9fa3df288bdf6c99
dwu1547 <dw1547@nyu.edu>
2019-04-20 01:45:16 -0400

    FirstCommit

---
+++ dwuCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dwuCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
